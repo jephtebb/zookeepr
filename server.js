0cbe75f376c06d27edd2d423397a8c583a8b8d5e
Jephtebb <jephteborno@yahoo.com>
2021-01-15 23:04:56 -0500

    able to capture from the client side

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -96,6 +97,17 @@ app.get('/api/animals', (req, res) => {
     }
     return true;
   }
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 
 app.listen(PORT, ()=>{
   console.log(`API server now on port ${PORT}!`);
